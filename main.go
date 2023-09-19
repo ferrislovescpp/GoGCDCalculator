ede5be6ab1d9e0e869690ab73694512969025d45
ferris <71037737+ferrislovescpp@users.noreply.github.com>
2023-09-18 23:00:25 -0700

    updated main.go
    [+] no longer have to reload the program to enter another input

--- main.go
+++ main.go
@@ -27,46 +27,49 @@ func isValidChoice(choice string) bool {
 
 // this is the entry point of every program, "main"
 func main() {
-	// using Go's var keyword to declare variables, we declare variables for user choice and numbers
-	// to store the user's input for further processing
-	var choice string
-	var num1, num2 int
+	for {
+		// using Go's var keyword to declare variables, we declare variables for user choice and numbers
+		// to store the user's input for further processing
+		var choice string
+		var num1, num2 int
 
-	// using fmt.Print to display a message without a newlin to prompt the user for their choice
-	// to ask the user if they want to proceed with GCD calculation.
-	fmt.Print("do you want to calculate the gcd of two non-negative integers? (y/n) ")
+		// using fmt.Print to display a message without a newlin to prompt the user for their choice
+		// to ask the user if they want to proceed with GCD calculation.
+		fmt.Print("do you want to calculate the gcd of two non-negative integers? (y/n) ")
 
-	// using fmt.Scanln to read a line of input from the standard input
-	// read the user's choice to know whether to proceed with the rest of the program or not.
-	fmt.Scanln(&choice)
+		// using fmt.Scanln to read a line of input from the standard input
+		// read the user's choice to know whether to proceed with the rest of the program or not.
+		fmt.Scanln(&choice)
 
-	// validate the choice by calling the isValidChoice function and checking its return value
-	/// to ensure the program proceeds only if a valid choice is made.
-	if !isValidChoice(choice) {
-		fmt.Println("invalid choice. exiting.")
-		return
+		// validate the choice by calling the isValidChoice function and checking its return value
+		/// to ensure the program proceeds only if a valid choice is made.
+		if !isValidChoice(choice) {
+			fmt.Println("invalid choice. exiting.")
+			return
+		}
+
+		// by checking if choice equals "n" and using return to exit the program
+		// exit if the choice is 'n' because the user chose not to proceed.
+		if choice == "n" {
+			fmt.Println("exiting.")
+			return
+		}
+
+		// prompt the user to enter two numbers to get the numbers for which the GCD will be calculated
+		// using fmt.Print to display a message without a newline.
+		fmt.Print("enter two non-negative integers: ")
+		fmt.Scanln(&num1, &num2)
+
+		// read the numbers from the user to get the actual integers for GCD calculation
+		// using fmt.Scanln to read the integers from the standard input
+		if num1 < 0 || num2 < 0 {
+			fmt.Println("please enter non-negative integers. exiting.")
+			return
+		}
+
+		// calculate GCD and display the result
+		gcd := calculateGCD(num1, num2)
+		fmt.Printf("the gcd of %d and %d is %d\n", num1, num2, gcd)
 	}
-
-	// by checking if choice equals "n" and using return to exit the program
-	// exit if the choice is 'n' because the user chose not to proceed.
-	if choice == "n" {
-		fmt.Println("exiting.")
-		return
-	}
-
-	// prompt the user to enter two numbers to get the numbers for which the GCD will be calculated
-	// using fmt.Print to display a message without a newline.
-	fmt.Print("enter two non-negative integers: ")
-	fmt.Scanln(&num1, &num2)
-
-	// read the numbers from the user to get the actual integers for GCD calculation
-	// using fmt.Scanln to read the integers from the standard input
-	if num1 < 0 || num2 < 0 {
-		fmt.Println("please enter non-negative integers. exiting.")
-		return
-	}
-
-	// calculate GCD and display the result
-	gcd := calculateGCD(num1, num2)
-	fmt.Printf("the gcd of %d and %d is %d\n", num1, num2, gcd)
 }
+
